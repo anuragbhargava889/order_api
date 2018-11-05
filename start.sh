d22112a7c3271d9705fe22c865ab373cd25c7775
anuragbhargava <a.bhargava@wimionline.be>
2018-11-05 19:34:52 +0530

    changes in start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 
+set -eu
+
 # Installing Docker CE
 if [ ! -f /usr/bin/docker ]; then
     echo 'Installing Docker CE'
     # Install Docker CE from the Debian-based distributions repository
     sudo apt update
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+    sudo apt --no-install-recommends install apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
     sudo apt update
     apt-cache policy docker-ce
     sudo apt-get install -y docker-ce
-    sudo usermod -aG docker ${USER}
+    sudo usermod --append --groups docker "$USER"
+    sudo systemctl status docker
+    sudo systemctl enable docker
+    sudo service docker restart
+    printf '\nDocker installed successfully\n\n'
 else
     echo "Docker CE already installed.  Skipping..."
 fi
 
 # Installing Docker Compose
-if [ ! -f /usr/bin/docker-compose ]; then
+if [ ! -f /usr/local/bin/docker-compose ]; then
     echo 'Installing Docker Compose'
-    sudo curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 else
     echo "Docker Compose already installed.  Skipping..."
